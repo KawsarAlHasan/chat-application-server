0f037f55aadcf82cef0ca5a07006cbe929c4b5bf
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-11-18 14:57:49 +0600

    create user

---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const User = require('../models/User')
+
+// create user
+router.post('/', async (req, res) => {
+  try {
+    const { name, email, password, picture } = req.body
+    console.log(req.body)
+    const user = await User.create({ name, email, password, picture })
+    res.status(201).json(user)
+  } catch (e) {
+    let msg
+    if (e.code == 11000) {
+      msg = 'User already exists'
+    } else {
+      msg = e.message
+    }
+    res.status(400).json(msg)
+  }
+})
