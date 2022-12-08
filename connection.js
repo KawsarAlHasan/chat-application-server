408f38d28f2fc70b20f05dfe0c2bee560ba9d8af
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-12-09 02:00:23 +0600

    change env

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 mongoose.connect(
-  `mongodb+srv://love-messages:${process.env.DB_PW}@cluster0.pmj8vq0.mongodb.net/love-messages?retryWrites=true&w=majority`,
+  `mongodb+srv://love-messages:Eo2ZP60Wlkc3ogKE@cluster0.pmj8vq0.mongodb.net/love-messages?retryWrites=true&w=majority`,
   () => {
     console.log('connected to mongodb')
   },
